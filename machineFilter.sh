7dcd60243a7013e13b975114078520900020c504
Cristian <alvarezmartinez132@gmail.com>
2023-07-20 00:37:41 -0500

    Añado nuevos filtros para la busqueda de maquinas

--- machineFilter.sh
+++ machineFilter.sh
@@ -5,7 +5,7 @@ greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
 redColour="\e[0;31m\033[1m"
 blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
+vyellowColour="\e[0;33m\033[1m"
 purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
@@ -25,50 +25,114 @@ function helpPanel() {
   echo -e "\n${yellowColour}[+]${endColour}${greyColour} Uso:${endColour}"
   echo -e "\t${purpleColour}u)${endColour}${grayColour} Descargar o actualiza archovos necesarios${endColour}"
   echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por un nombre de maquina${endColour}"
-  echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrat panel de ayuda${endColour}\n"
-}  
+  echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por dirección IP${endColour}"
+  echo -e "\t${purpleColour}d)${endColour}${grayColour} Buscar por la dificultad de la máquina${endColour}"
+  echo -e "\t${purpleColour}y)${endColour}${grayColour} Obtener link de la resolución de la máquina en Youtube${endColour}"
+  echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar panel de ayuda${endColour}\n"
+}
 
-function updateFiles(){
+function updateFiles() {
+
  if [ ! -f bundle.js ]; then
-     tput civis
-     echo -e "\n${yellowColour}[+]${endColour}${grayColour} Descargando archivos necesarios...${endColour}"
-     curl -s $main_url > bundle.js
-     js-beautify bundle.js | sponge bundle.js
-     echo -e "\n${yellowColour}[+]${endColour}${grayColour} Todos los archivos han sido descargados${endColour}"
-     tput cnorm
-   else
-     tput civis
-     curl -s $main_url > bundle_temp.js
-     js-beautify bundle_temp.js | sponge bundle_temp.js
-     md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
-     md5_original_value=$(md5sum bundle.js | awk '{print $1}')
-     
-     if [ "$md5_temp_value" == "$md5_original_value" ]; then
-       echo "[+] No hay actualizaciones"
-       rm bundle_temp.js
-     else
-       echo "[+] Hay actualizaciones"
-     fi
- 
-     tput cnorm
-   fi 
- }
- 
- # Indicadores
- declare -i parameter_counter=0
- 
- while getopts "m:uh" arg; do
-   case $arg in
-     m) machineName=$OPTARG; let parameter_counter+=1;;
-     u) let parameter_counter+=2;;
-     h) ;;
-   esac
- done
- 
- if [ $parameter_counter -eq 1 ]; then
-   searchMachine $machineName
- elif [ $parameter_counter -eq 2 ]; then
-   updateFiles
- else
-   helpPanel
- fi
+   tput civis
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Descargando archivos necesarios...${endColour}"
+    curl -s $main_url > bundle.js
+    js-beautify bundle.js | sponge bundle.js
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Todos los archivos han sido descargados${endColour}"
+    tput cnorm
+  else
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Comprobando si hay actualizaciones pendientes${endColour}"
+    tput civis
+    curl -s $main_url > bundle_temp.js
+    js-beautify bundle_temp.js | sponge bundle_temp.js
+    md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+    md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+    
+    if [ "$md5_temp_value" == "$md5_original_value" ]; then
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} No hay actualizaciones${endColour}"
+      rm bundle_temp.js
+    else
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Se han encontrado actualizaciones${endColour}"
+      sleep 1 
+      rm bundle.js && mv bundle_temp.js bundle.js
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Los archivos se han actualizado${endColour}"
+    fi
+
+    tput cnorm
+  fi 
+}
+
+function searchMachine(){
+  #Esto es para obtener la cadena de texto que se le esta pasando como "argumento" a la funcion
+  machineName="$1"
+  #
+  machineName_checker="$(cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//')"
+  if [ "$machineName_checker" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la máquina${endColour}${blueColour} $machineName${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//'
+  else
+    echo -e "\n${redColour}[!] la máquina proporcionada no existe${endColour}\n"
+  fi
+}
+
+function searchIP(){
+  idAddress="$1"
+  machineName="$(cat bundle.js | grep "ip: \"$ipAddress\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')"
+  if [ "$machineName" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} La máquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${p
+urpleColour} $machineName${endColour}"
+  else
+    echo -e "\n${redColour}[!] la dirección IP proporcionada no existe${endColour}\n"
+  fi
+}
+
+function getYoutubeLink(){
+  machineName="$1"
+  youtubeLink="$(cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//' | grep youtube | awk 'NF{pri
+nt $NF}')"
+  if [ $machineName ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} EL tutorial para esta máquina esta en el siguiente enlace:${endColour}${blueColour} $youtubeLink${endColour}\n"
+  else
+    echo -e "\n${redColour}[!] la máquina proporcionada no existe${endColour}\n"
+  fi 
+}
+
+function getMachineDifficulty(){
+  difficulty="$1"
+  results_check="$(cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep name | awk 'NF{print $NF}'| tr -d '"' | tr -d ',' | column)"
+  if [ "$results_check" ]; then 
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Las máquinas con dificultad${endColour} ${blueColour}$difficulty${endColour}${grayColour} son:${endColour}\n"
+    cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep name | awk 'NF{print $NF}'| tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!] la dificultad indicada no existe${endColour}\n"
+  fi
+}
+
+# Indicadores
+declare -i parameter_counter=0
+
+while getopts "m:ui:y:d:h" arg; do
+  case $arg in
+    m) machineName="$OPTARG"; let parameter_counter+=1;;
+    u) let parameter_counter+=2;;
+    i) ipAddress="$OPTARG"; let parameter_counter+=3;;
+    y) machineName="$OPTARG"; let parameter_counter+=4;;
+    d) difficulty="$OPTARG"; let parameter_counter+=5;;
+    h) ;;
+  esac
+done
+
+if [ $parameter_counter -eq 1 ]; then
+  searchMachine $machineName
+elif [ $parameter_counter -eq 2 ]; then
+  updateFiles
+elif [ $parameter_counter -eq 3 ]; then
+  searchIP $ipAddress
+elif [ $parameter_counter -eq 4 ]; then
+  getYoutubeLink $machineName
+elif [ $parameter_counter -eq 5 ]; then
+  getMachineDifficulty $difficulty
+else
+  helpPanel
+fi
+
