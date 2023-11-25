0b2c65edb8a50f8cc6ebea7494c72629031da794
Cristian Álvarez <94159947+CristianAlvarezM@users.noreply.github.com>
2023-11-24 19:46:54 -0500

    Command fix portScan.sh

--- portScan.sh
+++ portScan.sh
@@ -9,5 +9,5 @@ function ctrl_c(){
 trap ctrl_c INT
 
 for port in $(seq 1 65535); do
-  (echo '' > /dev/tcp/127.0.0.1/$port) 2>/den/null && echo "[+] $port - OPEN" &
+  (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "[+] $port - OPEN" &
 done; wait
